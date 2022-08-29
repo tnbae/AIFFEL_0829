74d6c247a2f737ec2fbda9def03bd07d1e699fb0
tnbae <acehyun1014@gmail.com>
2022-08-29 12:23:18 +0900

    파이썬 1번 부터 20번

---
+++ code.py
@@ -0,0 +1,63 @@
+# 1번
+print("Hello World")
+# 2번 
+print("Mary\'s cosmetics")
+# 3번
+print('신씨가 소리질렀다. "도둑이야".')
+# 4번
+print("\"C:\\Windos\"")
+# 5번
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \t는 tab키를 대신하고 \n은 새로운 줄을 형성함
+# 6번 
+print("오늘은","일요일")
+# 둘 다 str 자료형이기 때문에 아무 문제 없이 출력이 잘 됨
+# 7번
+print("naver;kako;sk;samsung")
+# 8번
+print("naver\\kakao\\sk\\samsung")
+# 9번
+print("first",end="");print("second")
+# 10번
+print(5/3)
+# 11번
+삼성전자=50000
+총평가금액=삼성전자 * 10
+print(총평가금액)
+# 12번
+시가총액=298000000000000
+현재가=50000
+PER=15.79
+print(시가총액,type(시가총액))
+print(현재가,type(현재가))
+print(PER,type(PER))
+# 13번
+s= "hello"
+t= "python"
+print(s,t)
+# 14번
+print(2+2*3) #8
+# 15번
+a="132"
+print(type(a)) #<class 'str'>
+# 16번
+num_str="720"
+int(num_str)
+print(type(num_str))
+# 17번
+num =100
+str(num)
+print(type(num))
+# 18번
+문자열="15.79"
+float(문자열)
+print(type(문자열))
+# 19번
+year="2020"
+print(int(year)-1)
+print(int(year)-2)
+print(int(year)-3)
+# 20번
+month=48584
+total=month*36
+print(str(total)+"원")
